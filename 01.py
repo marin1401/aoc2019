964f2688613ed93b21090e03b310b3f6bdac4037
marin1401 <58388551+marin1401@users.noreply.github.com>
2019-12-01 12:32:36 +0100

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,23 @@
+#Day 01
+
+def fuel_calc(x):
+    return int(x)//3-2
+
+#Part 1
+with open('./01.txt') as myinput:
+    inputlines = myinput.readlines()
+fuel_required = 0
+for line in inputlines:
+    fuel_required += fuel_calc(line)
+print (fuel_required)
+
+#Part 2
+total_fuel = 0
+for line in inputlines:
+    fuel_required_for_each_module = 0
+    fuel_required = fuel_calc(line)
+    while fuel_required >= 0:
+        fuel_required_for_each_module += fuel_required
+        fuel_required = fuel_calc(fuel_required)
+    total_fuel += fuel_required_for_each_module
+print (total_fuel)
