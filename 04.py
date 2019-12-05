d5868c06b799bb0212e8afab93f9c2716ccbc3fe
marin1401 <58388551+marin1401@users.noreply.github.com>
2019-12-05 13:14:41 +0100

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,11 +1,11 @@
 #Day 04
-
+ 
 lower_limit = 246540
 upper_limit = 787419
-
-#Part 1
+ 
 numbers = lower_limit-1
-candidates = set()
+candidates1 = set()
+candidates2 = set()
 for i in range(lower_limit, upper_limit):
     numbers += 1
     numbers_str = str(numbers)
@@ -13,5 +13,12 @@ for i in range(lower_limit, upper_limit):
        numbers_str[3]<=numbers_str[4]<=numbers_str[5]:
            for j in range(5):
                if numbers_str[j]==numbers_str[j+1]:
-                   candidates.add(numbers)
-print(len(candidates))
+                   candidates1.add(numbers)
+                   if numbers_str.count(numbers_str[j])==2:
+                       candidates2.add(numbers)
+
+#Part 1
+print(len(candidates1))
+
+#Part 2
+print(len(candidates2))
