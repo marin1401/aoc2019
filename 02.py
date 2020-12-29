76354c1ab6921ee4db73569a4644db7dc380f529
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-29 20:02:03 +0100

    Update 02.py

--- 02.py
+++ 02.py
@@ -2,28 +2,31 @@
 
 with open('./02.txt') as myinput:
     inputlines = myinput.read().split(',')
+
 inputlines = [int(line) for line in inputlines]
 
-def program(original_input, x, y):
+def Intcode(original_input, x, y):
     a = original_input.copy()
     a[1] = x
     a[2] = y
     for i in range(0, len(a), 4):
         if a[i] == 1:
-            a[a[3+i]] = a[a[1+i]]+a[a[2+i]]
+            a[a[3+i]] = a[a[1+i]] + a[a[2+i]]
         elif a[i] == 2:
-            a[a[3+i]] = a[a[1+i]]*a[a[2+i]]
-        else:
-            break
+            a[a[3+i]] = a[a[1+i]] * a[a[2+i]]
     return a[0]
 
 #Part 1
-print(program(inputlines, 12, 2))
+
+print(Intcode(inputlines, 12, 2))
 
 #Part 2
-for x in range(100):
-    for y in range(100):
-        result = program(inputlines, x, y)
-        if result == 19690720:
-            print(100*x+y)
-            break
+
+def get_output(output, inputlines):
+    for noun in range(100):
+        for verb in range(100):
+            result = Intcode(inputlines, noun, verb)
+            if result == output:
+                return 100 * noun + verb
+
+print(get_output(19690720, inputlines))
