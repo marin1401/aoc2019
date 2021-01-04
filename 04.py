30c8d2bdec48aa9b71b20b1b3664ce3cd0febacc
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-04 12:29:23 +0100

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,24 +1,25 @@
 #Day 04
- 
-lower_limit = 246540
-upper_limit = 787419
- 
-numbers = lower_limit-1
-candidates1 = set()
-candidates2 = set()
+
+with open('./04.txt') as myinput:
+    limits = myinput.read()
+
+lower_limit, upper_limit = map(int, limits.split('-'))
+
+candidates_1 = set()
+candidates_2 = set()
 for i in range(lower_limit, upper_limit):
-    numbers += 1
-    numbers_str = str(numbers)
-    if numbers_str[0]<=numbers_str[1]<=numbers_str[2]<= \
-       numbers_str[3]<=numbers_str[4]<=numbers_str[5]:
-           for j in range(5):
-               if numbers_str[j]==numbers_str[j+1]:
-                   candidates1.add(numbers)
-                   if numbers_str.count(numbers_str[j])==2:
-                       candidates2.add(numbers)
+    n = str(i)
+    if n[0] <= n[1] <= n[2] <= n[3] <= n[4] <= n[5]:
+        for current_digit, next_digit in zip(n, n[1:]):
+            if current_digit == next_digit:
+                candidates_1.add(n)
+                if n.count(current_digit) == 2:
+                    candidates_2.add(n)
 
 #Part 1
-print(len(candidates1))
+
+print(len(candidates_1))
 
 #Part 2
-print(len(candidates2))
+
+print(len(candidates_2))
