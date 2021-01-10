47260175d6af182742016858a257f1f23b4fa16a
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-10 23:36:03 +0100

    Update 05.py

--- 05.py
+++ 05.py
@@ -151,3 +151,46 @@ print(program(inputlines, 1))
 
 #Part 2
 print(program(inputlines, 5))
+
+#Day 5
+
+with open('./05.txt') as myinput:
+    inputlines = myinput.read().split(',')
+
+intcode_program = [int(line) for line in inputlines]
+
+def computer(intcode_program, input_value):
+    ip = intcode_program.copy()
+    out = []
+    i = 0
+    while ip[i] != 99:
+        opcode = '0000' + str(ip[i])
+        if opcode[-1] == '1' or opcode[-1] == '2':
+            if opcode[-3] == '1':
+                fp = ip[1+i]
+            else:
+                fp = ip[ip[1+i]]
+            if opcode[-4] == '1':
+                sp = ip[2+i]
+            else:
+                sp = ip[ip[2+i]]
+            if opcode[-5] == '1':
+                if opcode[-1] == '1':
+                    ip[3+i] = fp + sp
+                elif opcode[-1] == '2':
+                    ip[3+i] = fp * sp
+            else:
+                if opcode[-1] == '1':
+                    ip[ip[3+i]] = fp + sp
+                elif opcode[-1] == '2':
+                    ip[ip[3+i]] = fp * sp
+            i += 4
+        elif opcode[-1] == '3':
+            ip[ip[1+i]] = input_value
+            i += 2
+        elif opcode[-1] == '4':
+            out.append(ip[ip[1+i]])
+            i += 2
+    return out[-1]
+
+print(computer(intcode_program, 1))
