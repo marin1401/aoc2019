c9b67e10eb51d09ab3d1716e0fb2ed5d5d2281c9
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-23 15:55:11 +0100

    Update 06.py

--- 06.py
+++ 06.py
@@ -3,30 +3,34 @@
 with open('./06.txt') as myinput:
     inputlines = myinput.readlines()
 
-#Part 1    
-def count_orbits(obj, counter_a, counter_b):
-    counter_a += 1
-    counter_b.append(counter_a)
-    for line in inputlines:
-        if obj == line[:3]:
-            count_orbits(line[4:7], counter_a, counter_b)
+orbits = [line.strip().split(')') for line in inputlines]
 
-counter_a = -1
-counter_b = []
-count_orbits('COM', counter_a, counter_b)
-print(sum(counter_b))
+#Part 1
+
+def count_orbits(current_object, counter, counters):
+    counter += 1
+    counters.append(counter)
+    for object_1, object_2 in orbits:
+        if current_object == object_1:
+            count_orbits(object_2, counter, counters)
+
+counters = []
+count_orbits('COM', -1, counters)
+
+print(sum(counters))
 
 #Part 2
-def orbital_transfers_req(obj, orbit_map):
-    for line in inputlines:
-        if obj == line[4:7]:
-            orbit_map.add(line[:3])
-            orbital_transfers_req(line[:3], orbit_map)
+
+def required_orbital_transfers(current_object, orbit_map):
+    for object_1, object_2 in orbits:
+        if current_object == object_2:
+            orbit_map.add(object_1)
+            required_orbital_transfers(object_1, orbit_map)
 
 orbit_map_san = set()
-orbital_transfers_req('SAN', orbit_map_san)
+required_orbital_transfers('SAN', orbit_map_san)
 
 orbit_map_you = set()
-orbital_transfers_req('YOU', orbit_map_you)
+required_orbital_transfers('YOU', orbit_map_you)
 
 print(len(orbit_map_san.symmetric_difference(orbit_map_you)))
