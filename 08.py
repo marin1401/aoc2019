efe948cf389c9a1c7c16f1c8f6f13d4bbbb49bb0
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-05-23 16:23:38 +0200

    Update 08.py

--- 08.py
+++ 08.py
@@ -1,39 +1,35 @@
 #Day 08
 
 with open('./08.txt') as myinput:
-    imagelayers = myinput.read()
+    image_layers = myinput.read()
 
-imagewidth = 25
-imageheight = 6
-imagesize = imagewidth*imageheight
+image_width = 25
+image_height = 6
+image_size = image_width * image_height
 
 #Part 1
-layer_list = []
-layers_list = []
+
+layer, layers, zeros = [], [], []
 counter = 0
-number_of_zeros = []
-for digit in range(len(imagelayers)):
+for digit in image_layers:
+    layer.append(digit)
     counter += 1
-    layer_list.append(imagelayers[digit])
-    if counter % imagesize == 0:
-        layers_list.append(layer_list)
-        number_of_zeros.append(layer_list.count('0'))
-        layer_list = []
-layer_num_with_fewest_zeros = number_of_zeros.index(min(number_of_zeros))
-layer_with_fewest_zeros = layers_list[layer_num_with_fewest_zeros]
-print(layer_with_fewest_zeros.count('1')* \
-      layer_with_fewest_zeros.count('2'))
+    if counter % image_size == 0:
+        layers.append(layer)
+        zeros.append(layer.count('0'))
+        layer = []
+
+layer_with_fewest_zeros = layers[zeros.index(min(zeros))]
+
+print(layer_with_fewest_zeros.count('1') * layer_with_fewest_zeros.count('2'))
 
 #Part 2
-message = layers_list[len(layers_list)-1]
-for each_layer in reversed(layers_list):
-    for each_digit in range(len(each_layer)):
-        if each_layer[each_digit] != '2':
-            message[each_digit] = each_layer[each_digit]
-message_joined = ''.join(message)
-for line in range(0, len(message_joined), imagewidth):
-    print(message_joined[line:line+imagewidth].replace('0', ' '))
 
-
-
-            
+message = layers[-1]
+for layer in reversed(layers):
+    for digit in range(image_size):
+        if layer[digit] != '2':
+            message[digit] = layer[digit]
+message = ''.join(message)
+for line in range(0, image_size, image_width):
+    print(message[line:line+image_width].replace('0', ' '))
